777ae29f8210e96a03039ea7291a64b7e7c776d2
ZhdanovSergey <cynic85@mail.ru>
2022-07-12 14:52:33 +1000

    Initial commit

---
+++ src/main/java/com/prepkata/springjavaprepkata/SpringJavaPrePKataApplication.java
@@ -0,0 +1,13 @@
+package com.prepkata.springjavaprepkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaPrePKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaPrePKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prepkata/springjavaprepkata/SpringJavaPrePKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prepkata.springjavaprepkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaPrePKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
